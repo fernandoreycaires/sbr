5cbe6b241ea383556045d0329f5d357d6009420e
Fernando Rey Caires <fernando.rey.caires@gmail.com>
2020-11-29 18:30:53 -0300

    Removido algumas informações desnecessarias

--- storage/framework/views/34ba32fc7a801ea989e9912622ed9395e9fa5011.php
+++ storage/framework/views/34ba32fc7a801ea989e9912622ed9395e9fa5011.php
@@ -2,76 +2,9 @@
 
 <!-- Main content -->
 <section class="content">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3>R$</h3>
+      <p>Tela inicial, se eu tiver saco programo algo para aparecer aqui ..!!..!! </p>
 
-                    <p>Financeiro</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-cash"></i>
-                </div>
-                <a href="#" class="small-box-footer"> Visualizar <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-green">
-                <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                    <p>Bounce Rate</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="#" class="small-box-footer">Visualizar <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-yellow">
-                <div class="inner">
-                    <h3>4</h3>
-
-                    <p>Colaboradores</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-users"></i>
-                </div>
-                <a href=" <?php echo e(route('usuarios')); ?> " class="small-box-footer">Visualizar <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-red">
-                <div class="inner">
-                    <h3>65</h3>
-
-                    <p>Unique Visitors</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-pie-graph"></i>
-                </div>
-                <a href="#" class="small-box-footer">Visualizar <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-
-        </div>
-    <!-- /.row -->
-
-
-
-
-  </section>
+</section>
   <!-- /.content -->
 
 <?php $__env->stopSection(); ?>
